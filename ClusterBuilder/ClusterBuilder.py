15c6f3ab87ce9bcd6e946978f289de2ee645f4f1
jpatel@pivotal.io <jpatel@pivotal.io>
2016-10-31 12:59:25 -0700

    making standy and access optional. Fixes #2

--- ClusterBuilder/ClusterBuilder.py
+++ ClusterBuilder/ClusterBuilder.py
@@ -98,7 +98,6 @@ def buildServers(clusterDictionary):
         x.join()
     print clusterDictionary["clusterName"] + ": Cluster Configuration Complete"
     clusterDictionary["clusterNodes"] = clusterNodes
-    print(clusterDictionary)
     hostsFiles(clusterDictionary)
     keyShare(clusterDictionary)
 
@@ -121,15 +120,21 @@ def prepServer(clusterDictionary,clusterNode, nodeCnt):
     nodeName = clusterNode["nodeName"]
 
     # Set Server Role
+    if os.environ.get("STANDBY") == "yes" and os.environ.get("ACCESS") == "yes":
+        if (nodeCnt) == 0:
+            clusterNode["role"] = "access"
+        elif (nodeCnt) == 1:
+            clusterNode["role"] = "master1"
+        elif (nodeCnt) == 2:
+            clusterNode["role"] = "master2"
+        else:
+            clusterNode["role"] = "worker"
+    elif os.environ.get("STANDBY") == "no" and os.environ.get("ACCESS") == "no":
+        if (nodeCnt) == 0:
+            clusterNode["role"] = "master1"
+        else:
+            clusterNode["role"] = "worker"
 
-    if (nodeCnt) == 0:
-        clusterNode["role"] = "access"
-    elif (nodeCnt) == 1:
-        clusterNode["role"] = "master1"
-    elif (nodeCnt) == 2:
-        clusterNode["role"] = "master2"
-    else:
-        clusterNode["role"] = "worker"
     connected = False
     attemptCount = 0
     while not connected:
