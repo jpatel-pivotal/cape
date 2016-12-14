1b5b7a1794a451978d48b4377ce4251f79a88ed6
jpatel@pivotal.io <jpatel@pivotal.io>
2016-12-14 13:52:36 -0800

    added missing )

--- ClusterDestroyer/ClusterDestroyer.py
+++ ClusterDestroyer/ClusterDestroyer.py
@@ -1,5 +1,5 @@
-import os
 import sys
+import os
 import warnings
 import traceback
 import logging
@@ -7,7 +7,6 @@ from libcloud.compute.providers import get_driver
 from libcloud.compute.types import Provider
 from libcloud.compute.base import Node as node
 from libcloud.compute.drivers.dummy import DummyNodeDriver
-
 
 
 def destroyServers(clusterDictionary):
@@ -18,6 +17,11 @@ def destroyServers(clusterDictionary):
     print clusterDictionary["clusterName"] + ": Destroy Cluster Started"
     try:
 
+        logging.debug('SVC_ACCOUNT: ' + str(os.environ["SVC_ACCOUNT"]))
+        logging.debug('CONFIGS_PATH: ' + str(os.environ["CONFIGS_PATH"]))
+        logging.debug('SVC_ACCOUNT_KEY: ' + str(os.environ["SVC_ACCOUNT_KEY"]))
+        logging.debug('PROJECT: ' + str(os.environ["PROJECT"]))
+        logging.debug('ZONE: ' + str(os.environ["ZONE"]))
         ComputeEngine = get_driver(Provider.GCE)
         driver = ComputeEngine(os.environ["SVC_ACCOUNT"],
                                str(os.environ["CONFIGS_PATH"]) +
@@ -36,9 +40,11 @@ def destroyServers(clusterDictionary):
         logging.info('Deleting nodes that have ' +
                      clusterDictionary["clusterName"] +
                      ' in their name.')
-        delnodes = driver.ex_destroy_multiple_nodes(nodeList, ignore_errors=True,
-                                                   destroy_boot_disk=False,
-                                                   poll_interval=2, timeout=180)
+        delnodes = driver.ex_destroy_multiple_nodes(nodeList,
+                                                    ignore_errors=True,
+                                                    destroy_boot_disk=False,
+                                                    poll_interval=2,
+                                                    timeout=180)
         if (len(nodeList)) != (len(delnodes)):
             print clusterDictionary["clusterName"] + ": We may have deleted \
                 more nodes than expected!! "
@@ -49,10 +55,11 @@ def destroyServers(clusterDictionary):
             print clusterDictionary["clusterName"] + ": Destroyed Nodes"
             for i in range(0, int(len(nodeList))):
                 print "\t" + nodeList[i].name + ": " + str(delnodes[i])
-                logging.debug(nodeList[i].name + ": " + str(delnodes[i])
-
+                logging.debug(nodeList[i].name + ": " + str(delnodes[i]))
 
     except Exception as e:
         print e
         print traceback.print_exc()
+        logging.debug('Exception: ' + str(e))
+        logging.debug(traceback.print_exc())
         sys.exit("destroyServers Failed")
