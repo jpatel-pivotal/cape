dbc07a6080c8acd2123a22df9b5656595a8cd148
jpatel@pivotal.io <jpatel@pivotal.io>
2016-12-14 00:04:41 -0800

    implementing logging for querycluster

--- QueryCluster/QueryCluster.py
+++ QueryCluster/QueryCluster.py
@@ -1,14 +1,14 @@
 import os
-import time
 import warnings
 import traceback
+import logging
 from libcloud.compute.providers import get_driver
 from libcloud.compute.types import Provider
 
 
 
 def checkServerState(clusterDictionary):
-    # This methid checks if each node in the cluster is in RUNNING State
+    # This method checks if each node in the cluster is in RUNNING State
     # The most basic check is all we are doing here
     # We are using this before we destroy the cluster
     warnings.simplefilter("ignore")
@@ -24,11 +24,15 @@ def checkServerState(clusterDictionary):
         nodes = driver.list_nodes(ex_zone=str(os.environ["ZONE"]))
         if not nodes:
             print "Did not find any nodes for that cluster!"
+            logging.info('Did not find any nodes for that cluster')
         else:
             for node in nodes:
                 if clusterDictionary["clusterName"] in node.name:
                     print "\t" + node.name + ": " + node.state
+                    logging.debug(node.name + ": " + node.state)
 
     except Exception as e:
         print e
         print traceback.print_exc()
+        logging.debug('Exception: ' + str(e))
+        logging.debug(traceback.print_exc())
