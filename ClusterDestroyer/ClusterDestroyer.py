fff5fe95a6414d141f4f9ef1675bc7cf3e996616
jpatel@pivotal.io <jpatel@pivotal.io>
2016-12-14 00:12:21 -0800

    implementing logging for clusterDestroyer

--- ClusterDestroyer/ClusterDestroyer.py
+++ ClusterDestroyer/ClusterDestroyer.py
@@ -2,6 +2,7 @@ import os
 import sys
 import warnings
 import traceback
+import logging
 from libcloud.compute.providers import get_driver
 from libcloud.compute.types import Provider
 from libcloud.compute.base import Node as node
@@ -32,18 +33,23 @@ def destroyServers(clusterDictionary):
                 # print node
                 # print node.extra
 
-
+        logging.info('Deleting nodes that have ' +
+                     clusterDictionary["clusterName"] +
+                     ' in their name.')
         delnodes = driver.ex_destroy_multiple_nodes(nodeList, ignore_errors=True,
                                                    destroy_boot_disk=False,
                                                    poll_interval=2, timeout=180)
         if (len(nodeList)) != (len(delnodes)):
             print clusterDictionary["clusterName"] + ": We may have deleted \
                 more nodes than expected!! "
+            logging.debug('nodeList: ' + str(len(nodeList)))
+            logging.debug('delnodes: '_ str(len(delnodes)))
             print delnodes
         else:
             print clusterDictionary["clusterName"] + ": Destroyed Nodes"
             for i in range(0, int(len(nodeList))):
                 print "\t" + nodeList[i].name + ": " + str(delnodes[i])
+                logging.debug(nodeList[i].name + ": " + str(delnodes[i])
 
 
     except Exception as e:
